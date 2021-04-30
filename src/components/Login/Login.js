c12b0fe4d06bb61775afcfcb91e41a836911a0a9
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-30 17:28:21 -0600

    imported other images for the login page and styled it with styled components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,33 @@
 import React from "react";
 
-import { BgImage, Container, Content } from "./Login.styles";
+import {
+  BgImage,
+  Container,
+  Content,
+  CTA,
+  CTALogoOne,
+  SignUp,
+  Description,
+  CTALogoTwo,
+} from "./Login.styles";
 
 function Login() {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" />
+
+          <SignUp>GET ALL THERE</SignUp>
+
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" />
+        </CTA>
+
         <BgImage />
       </Content>
     </Container>
