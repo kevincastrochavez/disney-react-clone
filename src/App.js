ae305183651ede5656896aa63b0d87559dc04970
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-30 17:06:20 -0600

    created styles for the login component and imported the main background images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Login from "./components/Login/Login";
+
 import "./App.css";
 
 function App() {
-  return <div className="app">Disney clone</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { BgImage, Container, Content } from "./Login.styles";
+
+function Login() {
+  return (
+    <Container>
+      <Content>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+}
+
+export default Login;
